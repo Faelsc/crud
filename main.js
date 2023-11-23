a84beba7aff8f52dbcb0be280b97f5b3230cb1ca
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-11-23 16:02:34 -0300

    :sparkles: feat: criando read, delete e update client

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+    .classList.add('active');
 
 const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+    .classList.remove('active');
 
 const tempClient = {
     nome: "Rafael",
@@ -13,17 +13,31 @@ const tempClient = {
     cidade: "Muriaé"
 }
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
-const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+const deleteClient = (index) => {
+    const dbClient = readClient();
+    dbClient.splice(index, 1);
+    setLocalStorage(dbClient);
+} 
+
+const updateClient = (client) => {
+    const dbClient = readClient();
+    dbClient[index] = client;
+    setLocalStorage(dbClient);
+}
+
+const readClient = () => getLocalStorage()
 
 const createClient = (client) => {
-    const dbClient = getLocalStorage()
-    dbClient.push(client)
-    setLocalStorage(dbClient)    
+    const dbClient = getLocalStorage();
+    dbClient.push(client);
+    setLocalStorage(dbClient);
 }
 
 document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
+    .addEventListener('click', openModal);
 
 document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
+    .addEventListener('click', closeModal);
