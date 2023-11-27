0e143abca1161337822e5a57c15bcd9ac69a6abf
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-11-27 17:15:12 -0300

    :sparkles: feat: atualizar tabela

--- main.js
+++ main.js
@@ -8,13 +8,6 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active');    
 }
 
-const tempClient = {
-    nome: "Rafael",
-    email: "rafaelcorrea@gmail.com",
-    celular: "32123456789",
-    cidade: "Muriaé"
-}
-
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
 const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
 
@@ -56,9 +49,38 @@ const saveClient = () => {
             cidade: document.getElementById('cidade').value
         }
         createClient(client);
+        updateTable();
         closeModal();
     }
 }
+
+const createRow = (client) => {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type="button" class="button green">editar</button>
+            <button type="button" class="button red">excluir</button>
+        </td>
+    `
+    document.querySelector('#tableClient>tbody').appendChild(newRow);  
+}
+
+const clearTable = () => {
+    const rows = document.querySelectorAll('#tableClient>tbody tr');
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+const updateTable = () => {
+    const dbClient = readClient();
+    clearTable();
+    dbClient.forEach(createRow);
+}
+
+updateTable()
 
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal);
